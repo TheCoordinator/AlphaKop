25506318fbad5ab914943ef4949fd4fa7b79a7ec
Peyman Khanjan <p.khanjan@gmail.com>
2020-03-09 23:07:51 +0000

    Add Supreme Stock/Task.

---
+++ DaraConsole/src/Supreme/Entities/Stock.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace DaraBot.Supreme.Entities
+{
+    public partial struct Stock
+    {
+        [JsonProperty("products_and_categories")]
+        public Dictionary<string, Item[]> Items { get; internal set; }
+
+        [JsonProperty("release_week")]
+        public string? ReleaseWeek { get; internal set; }
+
+        [JsonConstructor]
+        public Stock(Dictionary<string, Item[]> items,
+                     string? releaseWeek)
+        {
+            Items = items;
+            ReleaseWeek = releaseWeek;
+        }
+    }
+
+    public partial struct Item
+    {
+        [JsonProperty("id")]
+        public long Id { get; }
+
+        [JsonProperty("name")]
+        public string Name { get; internal set; }
+
+        [JsonProperty("new_item")]
+        public bool? NewItem { get; internal set; }
+
+        [JsonProperty("category_name")]
+        public string? CategoryName { get; internal set; }
+
+        [JsonConstructor]
+        public Item(long id, string name, bool? newItem, string? categoryName)
+        {
+            Id = id;
+            Name = name;
+            NewItem = newItem;
+            CategoryName = categoryName;
+        }
+    }
+
+    public partial struct Stock
+    {
+        public static Stock FromJson(string json) => JsonConvert.DeserializeObject<Stock>(json, Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters = {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
---
+++ DaraConsole/src/Supreme/Entities/Task.cs
@@ -0,0 +1,32 @@
+using DaraBot.Core.Entities.User;
+
+namespace DaraBot.Supreme.Entities
+{
+    public struct Task
+    {
+        public UserProfile Profile { get; internal set; }
+        public string? CategoryName { get; internal set; }
+        public string? keywords { get; internal set; }
+        public string? style { get; internal set; }
+        public string? size { get; internal set; }
+        public int CheckoutDelay { get; internal set; }
+        public int RetryDelay { get; internal set; }
+
+        public SupremeTask(UserProfile profile,
+                           string categoryName,
+                           string keywords,
+                           string style,
+                           string size,
+                           int checkoutDelay = 1000,
+                           int retryDelay = 1000)
+        {
+            Profile = profile;
+            CategoryName = categoryName;
+            this.keywords = keywords;
+            this.style = style;
+            this.size = size;
+            CheckoutDelay = checkoutDelay;
+            RetryDelay = retryDelay;
+        }
+    }
+}
