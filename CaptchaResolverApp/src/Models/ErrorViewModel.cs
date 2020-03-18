085b8a8152f15279870efd02bb8285d419a5a920
Peyman Khanjan <p.khanjan@gmail.com>
2020-03-18 23:14:48 +0000

    Basic Node Electron app for CaptchaResolver.

--- CaptchaResolverApp/src/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace AlphaKop.CaptchaResolverApp.Models {
-    public class ErrorViewModel {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
