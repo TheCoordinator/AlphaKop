9a98e3c428785c5a6f98f9c745d41feaf89556b2
Peyman Khanjan <p.khanjan@gmail.com>
2020-04-05 23:22:41 +0100

    Use a better Cookie handler for Pooky.

--- Console/src/Supreme/Models/PookyCookies.cs
+++ Console/src/Supreme/Models/PookyCookies.cs
@@ -1,22 +1,27 @@
 using System.Collections.Generic;
+using System.Net;
+using AlphaKop.Supreme.Network.Converters;
 using Newtonsoft.Json;
 
 namespace AlphaKop.Supreme.Models {
     public struct PookyCookies {
         [JsonProperty("static")]
-        public IDictionary<string, string> StaticCookies { get; }
+        [JsonConverter(typeof(CookieJsonConverter))]
+        public IEnumerable<Cookie> StaticCookies { get; }
 
         [JsonProperty("atc")]
-        public IDictionary<string, string> AddToCartCookies { get; }
+        [JsonConverter(typeof(CookieJsonConverter))]
+        public IEnumerable<Cookie> AddToCartCookies { get; }
 
         [JsonProperty("checkout")]
-        public IDictionary<string, string> CheckoutCookies { get; }
+        [JsonConverter(typeof(CookieJsonConverter))]
+        public IEnumerable<Cookie> CheckoutCookies { get; }
 
         [JsonConstructor]
         public PookyCookies(
-            IDictionary<string, string> staticCookies,
-            IDictionary<string, string> addToCartCookies,
-            IDictionary<string, string> checkoutCookies
+            IEnumerable<Cookie> staticCookies,
+            IEnumerable<Cookie> addToCartCookies,
+            IEnumerable<Cookie> checkoutCookies
         ) {
             StaticCookies = staticCookies;
             AddToCartCookies = addToCartCookies;
--- Console/src/Supreme/Network/Extensions/PookyCookiesExtensions.cs
+++ Console/src/Supreme/Network/Extensions/PookyCookiesExtensions.cs
@@ -1,26 +1,20 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using AlphaKop.Supreme.Models;
 
 namespace AlphaKop.Supreme.Network.Extensions {
     static class PookyCookiesExtensions {
         public static string ToAddToCartCookiesString(this PookyCookies pookyCookies) {
-            var cookies = ToAddToCartCookies(pookyCookies);
-            var cookiesStringArray = cookies
-                .Select(pair => $"{pair.Key}={pair.Value}");
-
-            return string.Join("; ", cookiesStringArray);
-        }
-
-        public static IDictionary<string, string> ToAddToCartCookies(this PookyCookies pookyCookies) {
-            IDictionary<string, string>[] dictionaries = { 
-                pookyCookies.AddToCartCookies, pookyCookies.StaticCookies 
+            var cookies = new List<IEnumerable<Cookie>>() { 
+                pookyCookies.StaticCookies, pookyCookies.AddToCartCookies 
             };
-
-            return dictionaries
-                .SelectMany(dict => dict)
-                .ToLookup(pair => pair.Key, pair => pair.Value)
-                .ToDictionary(group => group.Key, group => group.FirstOrDefault());
+            
+            var cookiesStringArray = cookies
+                .SelectMany(cookie => cookie)
+                .Select(cookie => cookie.ToString());
+                
+            return string.Join("; ", cookiesStringArray);
         }
     }
 }
