c9206f128a8ebb4a7eca5cb9b7d5d83d614ffc38
Peyman Khanjan <p.khanjan@gmail.com>
2020-04-07 23:31:36 +0100

    Update CookiesExtensions.

--- Console/src/Supreme/Network/Extensions/CookiesExtensions.cs
+++ Console/src/Supreme/Network/Extensions/CookiesExtensions.cs
@@ -4,16 +4,12 @@ using System.Net;
 using AlphaKop.Supreme.Models;
 
 namespace AlphaKop.Supreme.Network.Extensions {
-    static class PookyCookiesExtensions {
-        public static string ToAddToCartCookiesString(this PookyCookies pookyCookies) {
-            var cookies = new List<IEnumerable<Cookie>>() { 
-                pookyCookies.StaticCookies, pookyCookies.AddToCartCookies 
-            };
-            
+    static class CookiesExtensions {
+        public static string ToCookiesString(this IEnumerable<IEnumerable<Cookie>> cookies) {
             var cookiesStringArray = cookies
                 .SelectMany(cookie => cookie)
                 .Select(cookie => cookie.ToString());
-                
+
             return string.Join("; ", cookiesStringArray);
         }
     }
