93863712d3042d2412f7be4000a1e794161f9086
Peyman Khanjan <p.khanjan@gmail.com>
2020-04-09 00:07:27 +0100

    Clean up.

--- Console/src/Supreme/Network/Extensions/CheckRequestFormExtensions.cs
+++ Console/src/Supreme/Network/Extensions/CheckRequestFormExtensions.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Net.Http;
 using AlphaKop.Core.CreditCard;
@@ -8,53 +9,56 @@ using FormValue = System.Collections.Generic.KeyValuePair<string, string>;
 namespace AlphaKop.Supreme.Network.Extensions {
     static class CheckoutRequestFormExtensions {
         public static FormUrlEncodedContent ToFormUrlEncodedContent(this ICheckoutRequest request, ICreditCardFormatter creditCardFormatter) {
-            var allValues = new ISet<FormValue>[] {
+            var allValues = new IEnumerable<FormValue>[] {
                 GetDefaultPageDataValues(request),
                 GetCookieSubValues(request),
                 GetCreditCardValues(request, creditCardFormatter: creditCardFormatter),
                 GetOrderProfileValues(request),
-                GetOrderAddressValues(request)
+                GetOrderAddressValues(request),
+                GetCaptchaValues(request)
             };
 
-            return new FormUrlEncodedContent(
-                allValues
+            var values = allValues
                     .SelectMany(value => value)
-                    .ToList()
-                    .OrderBy(value => value.Key)
-            );
+                    .OrderBy(value => value.Key);
+
+            var stringValue = string.Join('\n', values.Select(v => v.ToString()));
+
+            return new FormUrlEncodedContent(values);
         }
 
-        private static ISet<FormValue> GetDefaultPageDataValues(ICheckoutRequest request) {
+        private static IEnumerable<FormValue> GetDefaultPageDataValues(ICheckoutRequest request) {
             var result = (from mapping in request.Pooky.PageData.Mappings
                           where mapping.Mapping == null && mapping.Value != null
-                          select new FormValue(mapping.Name, mapping.Value ?? "")).ToHashSet();
+                          select new FormValue(mapping.Name, mapping.Value ?? "")).ToList();
 
+            result.RemoveAll(value => value.Key == "order[terms]");
             result.Add(new FormValue("order[terms]", "1"));
 
             return result;
         }
 
-        private static ISet<FormValue> GetCookieSubValues(ICheckoutRequest request) {
-            var jsonContent = $"\"{request.SizeId}\":{request.Quantity}";
+        private static IEnumerable<FormValue> GetCookieSubValues(ICheckoutRequest request) {
+            var jsonContent = $@"""{request.SizeId}"":{request.Quantity}";
 
             var json = "{ " + jsonContent + " }";
 
-            return new HashSet<FormValue>() {
+            return new FormValue[] {
                 new FormValue("cookie-sub", json)
             };
         }
 
-        private static ISet<FormValue> GetCaptchaValues(ICheckoutRequest request) {
+        private static IEnumerable<FormValue> GetCaptchaValues(ICheckoutRequest request) {
             var captcha = request.Captcha;
-            return new HashSet<FormValue>() {
+            return new FormValue[] {
                 new FormValue("g-recaptcha-response", captcha.Token)
             };
         }
 
-        private static ISet<FormValue> GetCreditCardValues(ICheckoutRequest request, ICreditCardFormatter creditCardFormatter) {
+        private static IEnumerable<FormValue> GetCreditCardValues(ICheckoutRequest request, ICreditCardFormatter creditCardFormatter) {
             var cardDetails = request.Profile.CardDetails;
 
-            return new HashSet<FormValue>() {
+            return new FormValue[] {
                 new FormValue("credit_card[cnb]", creditCardFormatter.FormatCardNumber(cardDetails.CardData)),
                 new FormValue("credit_card[month]", cardDetails.ExpiryMonth),
                 new FormValue("credit_card[ovv]", cardDetails.Verification),
@@ -63,26 +67,26 @@ namespace AlphaKop.Supreme.Network.Extensions {
             };
         }
 
-        private static ISet<FormValue> GetOrderProfileValues(ICheckoutRequest request) {
+        private static IEnumerable<FormValue> GetOrderProfileValues(ICheckoutRequest request) {
             var profile = request.Profile;
 
-            return new HashSet<FormValue>() {
+            return new FormValue[] {
                 new FormValue("order[email]", profile.Email),
                 new FormValue("order[tel]", profile.PhoneNumber)
             };
         }
 
-        private static ISet<FormValue> GetOrderAddressValues(ICheckoutRequest request) {
+        private static IEnumerable<FormValue> GetOrderAddressValues(ICheckoutRequest request) {
             var address = request.Profile.Address;
 
-            return new HashSet<FormValue>() {
+            return new FormValue[] {
                 new FormValue("order[billing_address]", address.LineOne),
                 new FormValue("order[billing_address_2]", address.LineTwo ?? ""),
                 new FormValue("order[billing_address_3]", address.LineThree ?? ""),
                 new FormValue("order[billing_city]", address.City),
                 new FormValue("order[billing_country]", address.CountryCode),
                 new FormValue("order[billing_name]", address.FullName),
-                new FormValue("order[billing_zip]", address.PostCode),
+                new FormValue("order[billing_zip]", address.PostCode)
             };
         }
     }
--- Console/src/Supreme/Network/Extensions/CookiesExtensions.cs
+++ Console/src/Supreme/Network/Extensions/CookiesExtensions.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Net;
 using AlphaKop.Supreme.Models;
@@ -8,9 +9,26 @@ namespace AlphaKop.Supreme.Network.Extensions {
         public static string ToCookiesString(this IEnumerable<IEnumerable<Cookie>> cookies) {
             var cookiesStringArray = cookies
                 .SelectMany(cookie => cookie)
+                .ToHashSet(new CookieNameComparer())
+                .ToList()
+                .OrderBy(cookie => cookie.Name)
                 .Select(cookie => cookie.ToString());
 
             return string.Join("; ", cookiesStringArray);
+        }
+
+        private sealed class CookieNameComparer : IEqualityComparer<Cookie> {
+            public bool Equals([AllowNull] Cookie x, [AllowNull] Cookie y) {
+                if (x == y) {
+                    return true;
+                }
+
+                return x?.Name == y?.Name;
+            }
+
+            public int GetHashCode([DisallowNull] Cookie obj) {
+                return obj.Name.GetHashCode();
+            }
         }
     }
 }
